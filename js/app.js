277044e470a43dbe094b188be2129f8ddabba23c
Kira Hartlage <kira.hartlage@gmail.com>
2017-07-02 12:55:59 -0400

    added search button ID to all pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
-//Problem: Hints are shown even when form is valid
-//Solution: Hide and show them at appropriate times
+// Submit and Log-in buttons not yet implemented
+
+$("#submit").click(function() {
+  alert("Not implemented yet.");
+});
+
+$("#log-in").click(function() {
+  alert("Not implemented yet.");
+});
+
+// Log-in Form
+
+// Log-in button is disabled unless both username and password fields have values
+function isLogInFormComplete() {
+  $("#logInForm").keyup(function() {
+    console.log("here");
+    var empty = false;
+    if ($("#usernameLogIn").val() == '' || $("#passwordLogIn").val() == '') {
+      empty = true;
+    }
+    if (empty) {
+      $("#log-in").prop('disabled', true);
+    }
+    else {
+      $("#log-in").prop('disabled', false);
+    }
+  });
+};
+
+// Check if fields have values
+$("#usernameLogIn").keyup(isLogInFormComplete);
+$("#passwordLogIn").keyup(isLogInFormComplete);
+
+isLogInFormComplete();
+
+// Sign Up Form
 
 var $password = $("#password");
 var $confirmPassword = $("#confirm_password");
